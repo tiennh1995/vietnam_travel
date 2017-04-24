142702476a7335f7bb842d7a781c4b114964e5f3
Lai Van Hai <haibka44@gmail.com>
2017-04-24 21:37:50 +0700

    upload-image

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -10,4 +10,20 @@ class ImagesController < ApplicationController
     end
   end
 
+  def create
+    @image = Image.new image_params
+    @image.user_id = current_user.id
+    
+    if @image.save
+      flash[:success] = "アップロードに成功しました"
+    else
+      flash[:danger] = "アップロードに失敗しました"
+    end
+    redirect_to root_path
+  end
+
+  private
+  def image_params
+    params.require(:image).permit :description, :image, :address, :category_id
+  end
 end

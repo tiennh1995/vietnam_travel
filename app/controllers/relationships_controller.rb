b01fd7d0acea82749d935055e212ea7cf99f7a0d
Nguyen Huu Tien <tiennh1995@gmail.com>
2017-05-09 12:31:39 +0700

    fix follow

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,19 +1,21 @@
 class RelationshipsController < ApplicationController
+  before_action :load_user
+
   def create
-    @user = User.find(params[:followed_id])
-    current_user.follow(@user)
-    respond_to do |format|
-      format.html { redirect_to @user }
-      format.js
-    end
+    @relationship = current_user.follow @user
   end
 
   def destroy
-    @user = Relationship.find(params[:id]).followed
-    current_user.unfollow(@user)
-    respond_to do |format|
-      format.html { redirect_to @user }
-      format.js
+    current_user.unfollow @user
+    @relationship = current_user.active_relationships.build
+  end
+
+  private
+  def load_user
+    @user = User.find_by id: params[:user_id]
+    unless @user
+      flash[:danger] = "ユーザーは存在じゃない。"
+      redirect_to root_path
     end
   end
 end
